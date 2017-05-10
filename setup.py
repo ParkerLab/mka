fd916fb6a6bcd43c6a6c4c7fde5ac7b6091f7df1
John Hensley <hensley@umich.edu>
2017-05-10 18:40:45 -0400

    Remove wiki integration; fix author

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = []
 
 setup(
     name='mka',
-    version='0.7.2',
+    version='0.7.3',
     description="Generates analysis pipelines from templates.",
     long_description=readme + '\n\n',
     author="The Parker Lab",
